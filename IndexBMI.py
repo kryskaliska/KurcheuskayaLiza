2495be8bd96b825e48b80d4de6ba1e33b665e1f6
Kurcheuskaya Lizaveta <kryska.liska@gmail.com>
2020-09-19 11:57:36 +0300

    Edit IndexBMI

--- IndexBMI.py
+++ IndexBMI.py
@@ -1,6 +1,6 @@
 ves = int(input('Какой у Вас вес в килограммах? '))
 rost = float(input('Какой у Вас рост в метрах? '))
 BMI = round(ves / (rost**2))
-print('Ваш индекс массы тела: ', BMI)
-a = BMI - 20
-scale = "20" + "="*a + str(BMI) + "="*(30-a) + "50"
+print('Ваш индекс массы тела: ', bmi)
+a = bmi - 20
+scale = "20" + "="*a + str(bmi) + "="*(30-a) + "50"
