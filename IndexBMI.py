fa10957cd85d37f94fa09a9e4df2906cdd2724a5
Kurcheuskaya Lizaveta <kryska.liska@gmail.com>
2020-09-20 20:59:02 +0300

    Multiplayer BMI

--- IndexBMI.py
+++ IndexBMI.py
@@ -1,6 +1,7 @@
 ves = int(input('Какой у Вас вес в килограммах? '))
 rost = float(input('Какой у Вас рост в метрах? '))
-BMI = round(ves / (rost**2))
+bmi = round(ves / (rost**2))
 print('Ваш индекс массы тела: ', bmi)
-a = bmi - 20
-scale = "20" + "="*a + str(bmi) + "="*(30-a) + "50"
+a = bmi - 17
+scale = "16" + "="*a + str(bmi) + "="*(22 - a) + "40"
+print(scale)
